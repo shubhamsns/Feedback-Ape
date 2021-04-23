24aea10e73753aded5148912053aab95c7095a1b
Shubham Sananse <61004681+shubham-sns@users.noreply.github.com>
2021-04-23 18:46:52 +0530

    feat: get site with swr & invalidate on create site (#4)

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,20 @@
+import useSWR from 'swr'
+
 import {useAuth} from '@/lib/auth'
+
 import {EmptyState} from '@/components/empty-state'
 import {SiteTableSkeleton} from '@/components/site-table-skeleton'
 import {DashboardShell} from '@/components/dashboard-shell'
 
+import {fetcher} from '@/utils/fetcher'
+import {SiteTable} from '@/components/site-table'
+
 function Dashboard() {
   const auth = useAuth()
 
-  if (!auth.user) {
+  const {data, error} = useSWR('/api/sites', fetcher)
+
+  if (!data) {
     return (
       <DashboardShell>
         <SiteTableSkeleton />
@@ -14,11 +22,7 @@ function Dashboard() {
     )
   }
 
-  return (
-    <DashboardShell>
-      <EmptyState />
-    </DashboardShell>
-  )
+  return <DashboardShell>{data.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}</DashboardShell>
 }
 
 export default Dashboard
