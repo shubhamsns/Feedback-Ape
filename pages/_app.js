c603761cdef254b4853efa8997718b6ac7990f77
Shubham Sananse <61004681+shubham-sns@users.noreply.github.com>
2021-04-25 01:30:53 +0530

    feat: SEO (#11)
    * styles: dashboardshell, homepage and site feedback
    
    * feat: SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,26 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import {ChakraProvider} from '@chakra-ui/react'
+import {DefaultSeo} from 'next-seo'
 
+import Head from 'next/head'
 import {AuthProvider} from '@/lib/auth'
+
 import {theme} from '@/styles/theme'
+import {SEO} from 'next-seo.config'
 
 function App({Component, pageProps}) {
   return (
-    <ChakraProvider theme={theme} resetCSS>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+
+      <ChakraProvider theme={theme} resetCSS>
+        <AuthProvider>
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ChakraProvider>
+    </>
   )
 }
 
